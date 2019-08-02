c32c1b4706b422944f9fd50ba6bc79472c1ab04a
김성겸/플랫폼개발팀 <sungkyum.kim@musinsa.com>
2019-08-02 11:32:23 +0900

    -

--- test/musinsa/js/ajax.js
+++ test/musinsa/js/ajax.js
@@ -4,12 +4,15 @@ mss.my = window.mss.my || {};
 mss.my.ajax = (function () {
     'use strict';
 
+    var config = mss.ui.config.get();
+
     var mask = '<div id="mask" style="position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;"></div>';
 
     var loadingImg = '' +
-        '<div id="loadingImg" style="position:fixed; left:50%; top:40%; display:none; z-index:10000;">' +
-        '<img src="/member/static/img/loading.gif"/>' +
+        '<div id="loadingImg" class="n-loading-page">' +
+        '<img src="' + config.storeHost + '/skin/musinsa/images/loading.png"/>' +
         '</div>';
+
 
     var fn = {
         showLoadingBar: function () {
